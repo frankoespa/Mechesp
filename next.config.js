ba3ff62154731afebfc8e728793afbae122499ba
Franco Esparza <frankoespa@gmail.com>
2020-04-09 15:58:03 -0300

    config: Add .env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const { GOOGLE_MAP_KEY } = require('./config');
+require('dotenv').config();
 
 module.exports = (phase, { defaultConfig }) => {
 	return {
 		env: {
-			GOOGLE_MAP_KEY,
-			API_GOOGLE_MAP: `https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ5eDTRPGrt5URTq156neafV8&key=${GOOGLE_MAP_KEY}&language=es`
+			GOOGLE_MAP_KEY: process.env.GOOGLE_MAP_KEY,
+			API_GOOGLE_MAP: `https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ5eDTRPGrt5URTq156neafV8&key=${process.env.GOOGLE_MAP_KEY}&language=es`
 		}
 	};
 };
