2d5fe031ac17b1a6c8e8210eed80344206dcf346
Franco Esparza <frankoespa@gmail.com>
2020-07-09 16:30:54 -0300

    fix: add sitemap.xml

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -1,10 +1,12 @@
 // Generate a sitemap.xml in out directory
 const sitemap = require('nextjs-sitemap-generator');
 
+// In Production
+if (process.env.GITHUB_WORKSPACE) {
 sitemap({
 	baseUrl: 'https://mecanicaesparza.com',
-	pagesDirectory: __dirname + '\\pages',
-	targetDirectory: 'out/'
+	pagesDirectory: process.env.GITHUB_WORKSPACE + '/pages',
+	targetDirectory: process.env.GITHUB_WORKSPACE + '/out'
 })
 	.then(() => {
 		console.log(`✅ sitemap.xml generated!`);
@@ -12,3 +14,8 @@ sitemap({
 	.catch((e) => {
 		console.log(e);
 	});
+} else {
+    console.log('sitemap.xml not generated')
+}
+
+
